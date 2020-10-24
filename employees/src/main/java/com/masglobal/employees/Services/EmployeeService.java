4c8d210f2c8d26369531532364e19d1286d4b330
code4jhon <code4jhon@gmail.com>
2020-10-24 08:12:07 -0500

    Consume external service for loadin employees, created service with factory method to create the required type of employee, used the service from within the controller to return the list of employees.

---
+++ employees/src/main/java/com/masglobal/employees/Services/EmployeeService.java
@@ -0,0 +1,38 @@
+package com.masglobal.employees.Services;
+
+import com.masglobal.employees.models.Contractor;
+import com.masglobal.employees.models.Employee;
+import com.masglobal.employees.models.InternalEmployee;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+
+public class EmployeeService {
+  private static Employee[] loadEmployees () {
+    final String uri = "http://masglobaltestapi.azurewebsites.net/api/employees";
+
+    RestTemplate restTemplate = new RestTemplate();
+    ResponseEntity<Employee[]> response = restTemplate.getForEntity(uri, Employee[].class);
+
+    return response.getBody();
+  }
+
+
+  public static ArrayList<Employee> getEmployees () {
+    Employee[] genericEmployees = loadEmployees();
+    ArrayList<Employee> employees = new ArrayList<>();
+
+    for (Employee employee : genericEmployees) {
+      if (employee.getContractTypeName().equals("MonthlySalaryEmployee")) {
+        InternalEmployee internalEmployee = new InternalEmployee(employee);
+        employees.add(internalEmployee);
+      } else if (employee.getContractTypeName().equals("HourlySalaryEmployee")) {
+        Contractor contractorEmployee = new Contractor(employee);
+        employees.add(contractorEmployee);
+      }
+    }
+
+    return employees;
+  }
+}
--- employees/src/main/java/com/masglobal/employees/controllers/EmployeeController.java
+++ employees/src/main/java/com/masglobal/employees/controllers/EmployeeController.java
@@ -1,7 +1,9 @@
 package com.masglobal.employees.controllers;
 
+import com.masglobal.employees.Services.EmployeeService;
 import com.masglobal.employees.models.Employee;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +15,11 @@ public class EmployeeController {
 
   @GetMapping
   public ArrayList<Employee> list() {
+    return EmployeeService.getEmployees();
+  }
 
-    return new ArrayList<Employee>();
+  @GetMapping("/{id}")
+  public Employee get(@PathVariable("id") long id) {
+    return new Employee();
   }
 }
